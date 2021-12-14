a33a8baf6a13e47dd5b108f27d977d800a736ac1
大井川真穂 <>
2021-12-14 20:25:59 +0900

    vue.configに書いたcssの設定を削除

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,11 @@ module.exports = {
     //build時にファイルのパスを正しい場所にする https://cli.vuejs.org/config/#global-cli-config
     publicPath: './',
 
-    css: {
-        loaderOptions: {
-            scss: {
-                additionalData: `@use "@/assets/css/main.scss";`
-            }
-        }
-    }
+    // css: {
+    //     loaderOptions: {
+    //         scss: {
+    //             additionalData: `@use "@/assets/css/main.scss";`
+    //         }
+    //     }
+    // }
 }
