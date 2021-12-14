0c6e2a0cc018262e1790d00a56889a887318eb27
大井川真穂 <>
2021-12-14 23:53:18 +0900

    ユーザートップページにscoped scssを追加

--- src/views/UserTop.vue
+++ src/views/UserTop.vue
@@ -61,3 +61,111 @@
   }
 
 </script>
+
+<style lang="scss" scoped>
+  @import "../assets/css/_base.scss";
+  @import "../assets/css/_modules.scss";
+
+  #userindex {
+      padding-top: 20px;
+      @include sp {
+          padding-top: 10px;
+      }
+  }
+
+  .personal-link {
+      padding-right: 80px;
+      margin: 0 auto 30px;
+      text-align: right;
+      
+      cursor: pointer;
+      @include sp {
+          margin: 0;
+          padding-right: 20px;
+      }
+      .icon {
+          padding-right: 5px;
+      }
+      .fa-check-circle, .editgoodsbtn {
+        color: $violet;
+      }
+  }
+  #addcampplan {
+      clear: both;
+      @include basic-btn;
+      border-radius: 0;
+      text-align: center;
+      margin: 0 auto;
+      width: 400px;
+      cursor: pointer;
+      @include sp {
+          width: 90%;
+      }
+      .fa-plus-circle {
+          font-size: 20px;
+          color:#fff
+      }
+      
+  }
+  #campcards {
+      background-color: lighten($violet, 30%);
+      padding: 40px;
+      margin: 50px 0;
+      .campcards-wrap{
+          @include flex;
+          padding: 0 40px;
+          .campcard {
+              width: 30%;
+              height: 160px;
+              border-radius: 8px;
+              box-shadow: 2px 2px 2px #777;
+              position: relative;
+              cursor: pointer;
+              margin-bottom: 30px;
+              img {
+                  width: 100%;
+                  height: 100%;
+                  object-fit: cover;
+                  border-radius: 8px;
+                  
+                  &:hover {
+                  opacity: 0.8;
+                  }
+              }
+              .campcard__text {
+                  text-align: center;
+                  color:#fff;
+                  position: absolute;
+                  top: 60px;
+                  width: 100%;
+                  z-index: 1;
+                  .campcard__place {
+                      font-size: 22px;
+                  }
+                  .campcard__date {
+                      font-size: 13px;
+                  }
+              }
+
+          }
+      }
+      //最後の行のスペース対策
+      .campcards-wrap:after {
+          content: "";
+          display: block;
+          width: 30%;
+      }
+      
+      @include sp {
+          padding: 20px;
+          margin: 20px 0;
+          .campcards-wrap {
+              padding: 0;
+              .campcard {
+                  width: 100%;
+                  margin-bottom: 20px;
+              }
+          }  
+      }
+  }
+</style>
