df8bb6bfd9b9082d972ef4f01de65196efa57fe3
大井川真穂 <>
2022-11-13 09:48:00 +0900

    dartsassに

--- src/components/LoginModal.vue
+++ src/components/LoginModal.vue
@@ -112,6 +112,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import "../assets/css/_base.scss";
-@import "../assets/css/_modules.scss";
+@use "@/assets/css/global" as global;
+@use "@/assets/css/modules" as modules;
 </style>
